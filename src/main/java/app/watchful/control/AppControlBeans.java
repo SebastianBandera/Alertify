f0f0ca4369ad581c7f5b4598829280b704b2b10c
Sebastián Bandera <sebastianbandera17@gmail.com>
2024-06-01 22:01:24 -0300

    WebRequest

--- src/main/java/app/watchful/control/AppControlBeans.java
+++ src/main/java/app/watchful/control/AppControlBeans.java
@@ -5,13 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import app.watchful.control.generic.SQLThreshold;
+import app.watchful.control.generic.WebRequest;
 
 @Configuration
 public class AppControlBeans {
 
 	@Bean(name = "sql_threshold")
 	@Scope("prototype")
-    public SQLThreshold myBean() {
+    public SQLThreshold SQLThreshold() {
         return new SQLThreshold();
+    }
+
+	@Bean(name = "web_request")
+	@Scope("prototype")
+    public WebRequest WebRequest() {
+        return new WebRequest();
     }
 }
---
+++ src/main/java/app/watchful/control/generic/WebRequest.java
@@ -0,0 +1,107 @@
+package app.watchful.control.generic;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import app.watchful.control.Control;
+import app.watchful.control.ControlResultStatus;
+import app.watchful.control.common.ObjectsUtils;
+
+public class WebRequest implements Control {
+	
+	public WebRequest() {
+		
+	}
+	
+	@Override
+	public Pair<Map<String, Object>, ControlResultStatus> execute(Map<String, Object> params) {
+		Objects.requireNonNull(params, "needs args to execute");
+		String url    = ObjectsUtils.noNull((String)params.get(Params.URL.getValue()), "").toLowerCase();
+		String method = ObjectsUtils.noNull((String)params.get(Params.METHOD.getValue()), "").toLowerCase();
+		
+		Map<String, Object> result = new HashMap<>();
+		boolean success = false;
+		
+		RestTemplate rt = new RestTemplate();
+		
+		HttpMethod httpMethod = parse(method);
+		
+		ResponseEntity<String> responseEntity = null;
+		
+		try {
+			responseEntity = rt.exchange(url, httpMethod, null, String.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		success = responseEntity != null && responseEntity.getStatusCodeValue() == 200;
+		
+		return Pair.of(result, ControlResultStatus.parse(success));
+	}
+	
+	private HttpMethod parse(String method) {
+		if(method == null) return HttpMethod.GET;
+		
+		method = method.toUpperCase();
+		
+		HttpMethod result = null;
+		
+		switch (method) {
+		case "GET":
+			result = HttpMethod.GET;
+			break;
+		case "POST":
+			result = HttpMethod.POST;
+			break;
+		case "PATCH":
+			result = HttpMethod.PATCH;
+			break;
+		case "HEAD":
+			result = HttpMethod.HEAD;
+			break;
+		case "PUT":
+			result = HttpMethod.PUT;
+			break;
+		case "OPTIONS":
+			result = HttpMethod.OPTIONS;
+			break;
+		case "TRACE":
+			result = HttpMethod.TRACE;
+			break;
+		case "DELETE":
+			result = HttpMethod.DELETE;
+			break;
+		default:
+			result = HttpMethod.GET;
+		}
+		
+		return result;
+	}
+
+	public enum Params {
+		URL("url"),
+		METHOD("method");
+
+		private String value;
+		
+		Params(String str) {
+			this.value = str;
+		}
+		
+		String getValue() {
+			return this.value;
+		}
+		
+		@Override
+		public String toString() {
+			return this.value;
+		}
+	}
+
+}
