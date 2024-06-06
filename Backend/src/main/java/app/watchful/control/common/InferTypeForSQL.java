10f732df4b26cb6b7c6de9a138c15ebb28e2651d
Sebastián Bandera <sbandera@anep.edu.uy>
2024-06-06 15:12:06 -0300

    InferTypeForSQL -> Short

--- Backend/src/main/java/app/watchful/control/common/InferTypeForSQL.java
+++ Backend/src/main/java/app/watchful/control/common/InferTypeForSQL.java
@@ -8,6 +8,7 @@ public class InferTypeForSQL {
 		if(object instanceof Integer) return Types.INTEGER;
 		if(object instanceof Double) return Types.DOUBLE;
 		if(object instanceof Float) return Types.FLOAT;
+		if(object instanceof Short) return Types.INTEGER;
 		if(object instanceof String) return Types.VARCHAR;
 		if(object instanceof java.util.Date) return Types.TIMESTAMP_WITH_TIMEZONE;
 		if(object instanceof java.sql.Date) return Types.TIMESTAMP_WITH_TIMEZONE;
