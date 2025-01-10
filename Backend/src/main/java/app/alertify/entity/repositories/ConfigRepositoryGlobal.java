e304cd8e5418253d0a3ff0381958c6f151d98726
Sebastián Bandera <sebastianbandera17@gmail.com>
2025-01-10 19:32:05 -0300

    log ConfigRepositoryGlobal

--- Backend/src/main/java/app/alertify/entity/repositories/ConfigRepositoryGlobal.java
+++ Backend/src/main/java/app/alertify/entity/repositories/ConfigRepositoryGlobal.java
@@ -2,11 +2,15 @@ package app.alertify.entity.repositories;
 
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConfigRepositoryGlobal {
+
+    private static final Logger log = LoggerFactory.getLogger(ConfigRepositoryGlobal.class);
 	
 	private final ConfigIntRepository configIntRepository;
     private final ConfigTextRepository configTextRepository;
@@ -20,14 +24,17 @@ public class ConfigRepositoryGlobal {
     }
     
     public Integer getInt(String paramName) {
+    	log.info("Read Integer parameter: " + paramName);
     	return configIntRepository.findById(paramName).map(data -> data.getConfigInt()).orElse(null);
     }
     
     public Date getDate(String paramName) {
+    	log.info("Read Date parameter: " + paramName);
     	return configTimestampRepository.findById(paramName).map(data -> data.getConfigTimestamp()).orElse(null);
     }
     
     public String getString(String paramName) {
+    	log.info("Read String parameter: " + paramName);
     	return configTextRepository.findById(paramName).map(data -> data.getConfigText()).orElse(null);
     }
     
