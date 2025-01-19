1c93d1b291b9723510bd4c2af595a086e557cd47
Sebastián Bandera <sebastianbandera17@gmail.com>
2025-01-19 18:48:52 -0300

    Status comp

---
+++ Frontend/src/app/pages/mainevents/status/status.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
+import { Status } from '../../../data/status';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-status',
+  imports: [CommonModule],
+  templateUrl: './status.component.html',
+  styleUrl: './status.component.css'
+})
+export class StatusComponent {
+  @Input() status!: Status | undefined;
+
+  @ViewChild('status_ok', { static: true }) status_ok!: TemplateRef<any>;
+  @ViewChild('status_warn', { static: true }) status_warn!: TemplateRef<any>;
+  @ViewChild('status_error', { static: true }) status_error!: TemplateRef<any>;
+
+  getTemplate(): TemplateRef<any> | null {
+    switch (this.status) {
+      case Status.OK:
+        return this.status_ok;
+      case Status.WARN:
+        return this.status_warn;
+      case Status.ERROR:
+        return this.status_error;
+      default:
+        return null;
+    }
+  }
+}
