6976ee3e0827d1b13e7be4ec193c4443b3a6b634
Sebastián Bandera <sebastianbandera17@gmail.com>
2025-04-15 13:43:27 -0300

    ButtonUpDownComponent changes

--- Frontend/src/app/pages/mainevents/button-up-down/button-up-down.component.ts
+++ Frontend/src/app/pages/mainevents/button-up-down/button-up-down.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
+import { FrontAlert } from '../../../data/front.dto';
+import { LoggerService } from '../../../services/logger.service';
 
 @Component({
   selector: 'app-button-up-down',
@@ -8,13 +10,35 @@ import { Component, TemplateRef, ViewChild } from '@angular/core';
   styleUrl: './button-up-down.component.css'
 })
 export class ButtonUpDownComponent {
-  private status_up: boolean = true;
+  private status_up: boolean | null = null;
+
+  @Input() alert?: FrontAlert;
 
   @ViewChild('boton_down', { static: true }) boton_down!: TemplateRef<any>;
   @ViewChild('boton_up', { static: true }) boton_up!: TemplateRef<any>;
+  @ViewChild('boton_null', { static: true }) boton_null!: TemplateRef<any>;
+  
+  constructor(private logger: LoggerService) {
+
+  }
+
+  ngOnInit(): void {
+    this.logger.debug('ButtonUpDownComponent ngOnInit ' + this.alert?.alert.name);
+    if(this.alert?.open == null) {
+      this.status_up = null;
+    } else {
+      this.status_up = !this.alert?.open;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.logger.debug('ButtonUpDownComponent ngOnDestroy ' + this.alert?.alert.name)
+  }
 
     getTemplate(): TemplateRef<any> | null {
       switch (this.status_up) {
+        case null:
+          return this.boton_null;
         case true:
           return this.boton_up;
         case false:
