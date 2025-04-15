cf68c757278341a84325f225b3d13cbd853c0062
Sebastián Bandera <sebastianbandera17@gmail.com>
2025-04-15 13:37:33 -0300

    IndexedData encapsulation

--- Frontend/src/app/data/index.data.dto.ts
+++ Frontend/src/app/data/index.data.dto.ts
@@ -1,9 +1,28 @@
 export class IndexedData<K, V> {
-    index: Map<K, V>;
-    data: V[];
+    private index: Map<K, V>;
+    private data: V[];
 
     constructor() {
         this.index = new Map<K, V>();
         this.data = [];
+    }
+
+    get getAllData(): V[] {
+        return this.data;
+    }
+
+    get getIndex(): Map<K, V> {
+        return this.index;
+    }
+
+    public setIndexed(item: V, keyGetter: (arg: V) => K): void {
+        if (item != null && keyGetter != null) {
+          const id: K = keyGetter(item);
+          if (!this.index.has(id)) {
+            this.data.push(item);
+          }
+    
+          this.index.set(id, item);
+        }
     }
 }
