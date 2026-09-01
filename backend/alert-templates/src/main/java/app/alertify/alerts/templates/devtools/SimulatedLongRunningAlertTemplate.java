276e4f494941a9ec9611911f13d31505fd1d6a0f
Sebastián Bandera <sebastianbandera17@gmail.com>
2026-09-01 18:46:03 -0300

    SimulatedLongRunningAlertTemplate allowConcurrentExecutions = true

--- backend/alert-templates/src/main/java/app/alertify/alerts/templates/devtools/SimulatedLongRunningAlertTemplate.java
+++ backend/alert-templates/src/main/java/app/alertify/alerts/templates/devtools/SimulatedLongRunningAlertTemplate.java
@@ -17,7 +17,8 @@ import app.alertify.alerts.template.annotation.AlertTemplate;
 @AlertTemplate(
     nameKey = "alerts.template.devtools.simulatedLongRunning.name",
     descriptionKey = "alerts.template.devtools.simulatedLongRunning.description",
-    sourcePath = "app/alertify/alerts/templates/devtools/SimulatedLongRunningAlertTemplate.java"
+    sourcePath = "app/alertify/alerts/templates/devtools/SimulatedLongRunningAlertTemplate.java",
+    allowConcurrentExecutions = true
 )
 public final class SimulatedLongRunningAlertTemplate implements AlertEvaluator {
 
